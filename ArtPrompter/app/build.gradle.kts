945e0251cdbd6792484e1d34a0be4e7672521a16
dcmoote <dcmoote@gmail.com>
2026-04-16 10:01:01 -0600

    Updated version number for production

--- ArtPrompter/app/build.gradle.kts
+++ ArtPrompter/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.dcmoote.inkwell"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1.0"
+        versionCode = 7
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
