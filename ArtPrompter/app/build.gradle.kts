cd692b19fb7afee7877f50a9dfb7dfb9567b0c71
dcmoote <dcmoote@gmail.com>
2026-04-16 12:28:10 -0600

    Updated privacy policy

--- ArtPrompter/app/build.gradle.kts
+++ ArtPrompter/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.dcmoote.inkwell"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.1.1"
+        versionCode = 9
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
